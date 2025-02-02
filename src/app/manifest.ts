4234f4b5718a6bfcfbb6625f17adf74cb51b2120
James Taranto <taranto.james@gmail.com>
2025-02-02 21:18:00 +1100

    add share target

---
+++ src/app/manifest.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "Next.js PWA",
+    short_name: "NextPWA",
+    description: "A Progressive Web App built with Next.js",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#000000",
+    icons: [],
+    share_target: { action: "/recipe", method: "GET", params: { url: "url" } },
+  };
+}
