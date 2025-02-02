5d7f6e26494de2c51b7ff1c14bcb287c700a850a
James Taranto <taranto.james@gmail.com>
2025-02-03 01:26:13 +1100

    loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
 import { FormButton } from "@/components/submit-recipe-url";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import Form from "next/form";
 
 export const runtime = "edge";
 
 export default function Home() {
   return (
-    <Form className="flex" action="/recipe">
-      <Input type="url" name="url" />
-      <FormButton />
-    </Form>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <Form className="flex flex-col gap-6" action="/recipe">
+          <div className="grid gap-2">
+            <Label htmlFor="url">Recipe Link</Label>
+            <Input
+              type="url"
+              id="url"
+              name="url"
+              placeholder="https://www.recipetineats.com/dal-indian-lentil-curry/"
+            />
+          </div>
+          <FormButton />
+        </Form>
+      </div>
+    </div>
   );
 }
