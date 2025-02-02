5a9e5e3fc70e5e953314c3c4317462f5be6f8baf
James Taranto <taranto.james@gmail.com>
2025-02-02 19:04:35 +1100

    comment out cloudflare env for now

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
-interface CloudflareEnv {
-    // MY_KV_1: KVNamespace;
-    // MY_KV_2: KVNamespace;
-    // MY_R2: R2Bucket;
-    // MY_DO: DurableObjectNamespace;
-}
+// interface CloudflareEnv {
+//     // MY_KV_1: KVNamespace;
+//     // MY_KV_2: KVNamespace;
+//     // MY_R2: R2Bucket;
+//     // MY_DO: DurableObjectNamespace;
+// }
