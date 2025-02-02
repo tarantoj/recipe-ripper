7579a404811a1cb66226c32314f74940234cdc13
James Taranto <taranto.james@gmail.com>
2025-02-02 21:39:55 +1100

    update sizes and purpose

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -10,7 +10,12 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#ffffff",
     theme_color: "#000000",
     icons: [
-      { src: "/icon.svg", type: "image/svg+xml", sizes: "192x192 512x512" },
+      {
+        src: "/icon.svg",
+        type: "image/svg+xml",
+        sizes: "48x48 72x72 96x96 128x128 256x256 512x512",
+        purpose: "any",
+      },
     ],
     share_target: { action: "/recipe", method: "GET", params: { url: "url" } },
   };
