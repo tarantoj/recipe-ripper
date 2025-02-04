cf6d57c5d25c87093ccf5941b2667be1a3ab2a1c
James Taranto <taranto.james@gmail.com>
2025-02-04 20:09:52 +1100

    remove wrangler from devenv

--- devenv.nix
+++ devenv.nix
@@ -9,7 +9,7 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [pkgs.git pkgs.wrangler];
+  packages = [pkgs.git ];
 
   # https://devenv.sh/languages/
   languages.javascript.enable = true;
