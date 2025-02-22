553da2fbe36874b819e2a375cdd1360f5d1ba630
James Taranto <taranto.james@gmail.com>
2025-02-23 02:25:05 +1100

    add render cache

--- src/lib/request-cache.ts
+++ src/lib/request-cache.ts
@@ -1,34 +1,37 @@
 "use server";
 import { getRequestContext } from "@cloudflare/next-on-pages";
+import { cache } from "react";
 
-export const requestCache = async <T>(fn: () => Promise<T>, key: string) => {
-  const existingKV =
-    await getRequestContext().env.RECIPE_CACHE.getWithMetadata(key);
+export const requestCache = cache(
+  async <T>(fn: () => Promise<T>, key: string) => {
+    const existingKV =
+      await getRequestContext().env.RECIPE_CACHE.getWithMetadata(key);
 
-  const existing = JSON.parse(existingKV.value ?? "null") as T | null;
-  const existingMetadata = JSON.parse(
-    (existingKV.metadata as string) ?? "null",
-  ) as { expiry?: number } | null;
+    const existing = JSON.parse(existingKV.value ?? "null") as T | null;
+    const existingMetadata = JSON.parse(
+      (existingKV.metadata as string) ?? "null",
+    ) as { expiry?: number } | null;
 
-  const refresh = async () => {
-    const fresh = await fn();
-    if (!fresh) return;
+    const refresh = async () => {
+      const fresh = await fn();
+      if (!fresh) return;
 
-    getRequestContext().env.RECIPE_CACHE.put(key, JSON.stringify(fresh), {
-      metadata: JSON.stringify({
-        expiry: Date.now() + 1000 * 60 * 60 * 24 * 7 * 31, // One month
-      }),
-    });
+      getRequestContext().env.RECIPE_CACHE.put(key, JSON.stringify(fresh), {
+        metadata: JSON.stringify({
+          expiry: Date.now() + 1000 * 60 * 60 * 24 * 7 * 31, // One month
+        }),
+      });
 
-    return fresh;
-  };
+      return fresh;
+    };
 
-  if (existing && existingMetadata?.expiry) {
-    if (existingMetadata.expiry > Date.now()) {
-      refresh();
+    if (existing && existingMetadata?.expiry) {
+      if (existingMetadata.expiry > Date.now()) {
+        refresh();
+      }
+      return existing;
+    } else {
+      return await refresh();
     }
-    return existing;
-  } else {
-    return await refresh();
-  }
-};
+  },
+);
