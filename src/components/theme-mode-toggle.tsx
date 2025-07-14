5810686c540dcfe51e7848b35ba1fdce9b6cf46c
James Taranto <taranto.james@gmail.com>
2025-07-14 23:56:41 +1000

    fix hydration errors

--- src/components/theme-mode-toggle.tsx
+++ src/components/theme-mode-toggle.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import * as React from "react";
+import { useEffect, useState } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -13,7 +12,16 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ModeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <DropdownMenu>
